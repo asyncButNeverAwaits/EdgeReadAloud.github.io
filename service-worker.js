2ff52138359ae76fcf24a22748f37e75c939ce97
EdgeReadAloud <read-aloud@outlook.com>
2022-09-02 10:34:17 +0800

    up

--- service-worker.js
+++ service-worker.js
@@ -68,7 +68,7 @@ self.addEventListener('fetch', event => {
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2.4';
+const PRECACHE = 'precache-v2.5';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
