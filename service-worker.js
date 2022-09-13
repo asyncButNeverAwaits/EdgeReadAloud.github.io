c56e35a8665e82d17e834ae8cd02d862c1fc1073
chekwei.tan <chekwei.tan@consolsys.com>
2022-09-13 17:58:46 +0800

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,15 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2.15';
+const PRECACHE = 'precache-v2.16';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
-  // 'index.css',
   'scripts/bundle.js',
   'styles/index.css',
-  // 'indexedDb.js',
-  // 'loading.js',
-  // 'vendors/md5.umd.min.js',
-  // 'vendors/ifvisible.min.js',
 ];
 
 // The install handler takes care of precaching the resources we always need.
